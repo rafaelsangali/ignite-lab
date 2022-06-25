3fc0f9b72bcab4078579cf869407713c73089f2c
Rafael Sangali <rafaelsangali@outlook.com>
2022-06-24 23:11:39 -0300

    req não concluida

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ow0f2017p901z40giw2jho/master',
+  uri: import.meta.env.VITE_API_URL,
+  // headers:{
+  //   'Authorization': `Bearer ${import.meta.env.VITE_API_ACESS_TOKEN}`
+  // },
   cache: new InMemoryCache()
 })
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,6 +1,31 @@
+import { gql, useMutation } from "@apollo/client";
+import { useState, FormEvent } from "react";
 import { Logo } from "../components/Logo";
 
+const CREATE_SUBSCRIBER_MUTATION = gql`
+  mutation CreateSubscriber ($name:String!,$email:String!) {
+  createSubscriber(data: {name: $name, email: $email}) {
+    id
+  }
+}
+`
 export function Subscribe(){
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  const [createSubscriber] = useMutation(CREATE_SUBSCRIBER_MUTATION)
+
+  function handleSubscribe(event:FormEvent){
+    event.preventDefault()
+    createSubscriber({
+      variables:{
+        name,
+        email,
+      }
+    })
+    console.log('foi')
+  }
+
   return(
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
       <div className=" w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
@@ -16,16 +41,18 @@ export function Subscribe(){
             <strong className="text-2xl mb-6 block">
               Inscreva-se gratuitamente
             </strong>
-            <form action="" className="flex flex-col gap-2 w-full">
+            <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
               <input 
               type="text" 
               placeholder="Seu nome completo"
-              className="bg-gray-900 rounded px-5 h-14"  
+              className="bg-gray-900 rounded px-5 h-14"
+              onChange={ event => setName(event.target.value)}  
             />
               <input 
               type="email" 
               placeholder="Digite seu e-mail"
-               className="bg-gray-900 rounded px-5 h-14" 
+               className="bg-gray-900 rounded px-5 h-14"
+               onChange={ event => setEmail(event.target.value)} 
             />
             <button 
             type="submit"
